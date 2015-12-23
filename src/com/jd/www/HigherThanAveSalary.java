c1e6167264479f99e6728679ee8e26bedc9c38d4
qiu1157 <qiuxiangu@gmail.com>
2015-12-23 14:29:29 +0800

    列出工资比公司平均工资要高的员工姓名及其工资

--- src/com/jd/www/HigherThanAveSalary.java
+++ src/com/jd/www/HigherThanAveSalary.java
@@ -18,16 +18,20 @@ public class HigherThanAveSalary {
 		Job job = Job.getInstance(conf, "HigherThanAveSalary");
 		job.setJarByClass(com.jd.www.HigherThanAveSalary.class);
 		job.setMapperClass(MyMapper.class);
-
 		job.setReducerClass(MyReducer.class);
 
+		//必须设置Reduce任务数为1, 这样才能保证各Reduce是串行的
+		job.setNumReduceTasks(1);
+		//设置MAPPER输出格式
+		job.setMapOutputKeyClass(IntWritable.class);
+		job.setMapOutputValueClass(Text.class);
 		// TODO: specify output types
 		job.setOutputKeyClass(Text.class);
 		job.setOutputValueClass(Text.class);
 
 		// TODO: specify input and output DIRECTORIES (not files)
-		FileInputFormat.setInputPaths(job, new Path("src"));
-		FileOutputFormat.setOutputPath(job, new Path("out"));
+		FileInputFormat.setInputPaths(job, new Path("hdfs://master.hadoop:9000/in/emp"));
+		FileOutputFormat.setOutputPath(job, new Path("hdfs://master.hadoop:9000/out"));
 
 		if (!job.waitForCompletion(true))
 			return;		
@@ -43,17 +47,33 @@ public class HigherThanAveSalary {
 		protected void map(Object key, Text value, Context context)
 				throws IOException, InterruptedException {
 			String[] columns = value.toString().split(",");
-			context.write(new IntWritable(0), value);
+			context.write(new IntWritable(0), new Text(columns[5]));
+			context.write(new IntWritable(1), new Text(columns[1]+"+"+columns[5]));
 		}
 		
 	}
 	
 	public static class MyReducer extends Reducer <IntWritable, Text, Text, Text> {
-
+		private long allSalary = 0;
+		private int allEmpCount = 0;
+		private float aveSalary = 0;
+		
+		private long empSalary = 0;
 		@Override
 		protected void reduce(IntWritable key, Iterable<Text> values, Context context)
 				throws IOException, InterruptedException {
-			
+			for (Text value : values) {
+				if (0 == key.get()) {
+					allSalary += Long.parseLong(value.toString());
+					allEmpCount++ ;
+				}else if(1 == key.get()) {
+					aveSalary = allSalary/allEmpCount;
+					empSalary = Long.parseLong(value.toString().split("\\+")[1]);
+					if (empSalary > aveSalary) {
+						context.write(new Text(value.toString().split("\\+")[0]), new Text(empSalary+""));
+					}
+				}
+			}
 		}
 		
 	}
